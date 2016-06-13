19d35c26f2691554c097d686ab1b6188b6ee29d9
Professor Taki <Taki21@users.noreply.github.com>
2016-06-12 21:11:35 -0400

    Kits During Match Check

--- src/SurvivalGamesV4/SurvivalGamesV4.php
+++ src/SurvivalGamesV4/SurvivalGamesV4.php
@@ -75,119 +75,6 @@ class SurvivalGamesV4 extends PluginBase implements Listener {
 		$this->getServer()->getScheduler()->scheduleRepeatingTask(new RefreshSigns($this), 10);
 	}
 	
-	public function giveRandomKit(PlayerJoinEvent $e){
-		$config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
-		if($config->get("RandomKit") === true){	
-			$p = $e->getPlayer();
-			$kit = rand(1,10);
-			switch($kit){
-				case 1:
-					$p->getInventory()->addItem(Item::get(302,0,1));
-					$p->getInventory()->addItem(Item::get(303,0,1));
-					$p->getInventory()->addItem(Item::get(304,0,1));
-					$p->getInventory()->addItem(Item::get(305,0,1));
-					$p->getInventory()->addItem(Item::get(279,0,1));
-				
-					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."VIP+".C::DARK_AQUA." Kit!");
-				break;
-			
-				case 2:
-					$p->getInventory()->addItem(Item::get(298,0,1));
-					$p->getInventory()->addItem(Item::get(299,0,1));
-					$p->getInventory()->addItem(Item::get(300,0,1));
-					$p->getInventory()->addItem(Item::get(301,0,1));
-					$p->getInventory()->addItem(Item::get(268,0,1));
-				
-					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Beginnerz".C::DARK_AQUA." Kit!");
-				break;
-			
-				case 3:
-					$effect = Effect::getEffect(1);
-					$effect->setDuration(2184728365782365723642365723652); 
-					$effect->setVisible(true);
-					$effect->setAmplifier(2);
-					$p->addEffect($effect);
-					
-					$effect2 = Effect::getEffect(8);
-					$effect2->setDuration(2184728365782365723642365723652); 
-					$effect2->setVisible(true);
-					$effect2->setAmplifier(3);
-					$p->addEffect($effect2);
-				
-					$p->getInventory()->addItem(Item::get(311,0,1));
-				
-					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Athlete".C::DARK_AQUA." Kit!");
-				break;
-			
-				case 4:
-					$ef = Effect::getEffect(8);
-					$ef->setDuration(2184728365782365723642365723652); 
-					$ef->setVisible(true);
-					$ef->setAmplifier(4);
-					$p->addEffect($ef);
-				
-					$p->getInventory()->addItem(Item::get(293,0,1));
-				
-					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Rabbit".C::DARK_AQUA." Kit!");
-				break;
-				
-				case 5:
-					$p->getInventory()->addItem(Item::get(314,0,1));
-					$p->getInventory()->addItem(Item::get(315,0,1));
-					$p->getInventory()->addItem(Item::get(316,0,1));
-					$p->getInventory()->addItem(Item::get(317,0,1));
-					$p->getInventory()->addItem(Item::get(283,0,2));
-					$p->getInventory()->addItem(Item::get(322,0,5));
-				
-					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Midas".C::DARK_AQUA." Kit!");
-				break;
-			
-				case 6:
-					$p->getInventory()->addItem(Item::get(261,0,1));
-					$p->getInventory()->addItem(Item::get(262,0,64));
-					$p->getInventory()->addItem(Item::get(354,0,5));
-				
-					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Hyper Mongols".C::DARK_AQUA." Kit!");
-				break;
-				
-				case 7:
-					$p->getInventory()->addItem(Item::get(311,0,1));
-					
-					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Tank".C::DARK_AQUA." Kit!");
-				break;
-				
-				case 8:
-					$p->getInventory()->addItem(Item::get(293,0,1));
-					$p->getInventory()->addItem(Item::get(298,0,1));
-					$p->getInventory()->addItem(Item::get(299,0,1));
-					$p->getInventory()->addItem(Item::get(300,0,1));
-					$p->getInventory()->addItem(Item::get(301,0,1));
-					
-					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Ninja".C::DARK_AQUA." Kit!");
-				break;
-				
-				case 9:
-					$p->getInventory()->addItem(Item::get(293,0,1));
-					$p->getInventory()->addItem(Item::get(298,0,1));
-					$p->getInventory()->addItem(Item::get(299,0,1));
-					$p->getInventory()->addItem(Item::get(300,0,1));
-					$p->getInventory()->addItem(Item::get(301,0,1));
-					
-					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Ninja".C::DARK_AQUA." Kit!");
-				break;
-				
-				case 10:
-					$p->getInventory()->addItem(Item::get(306,0,1));
-					$p->getInventory()->addItem(Item::get(307,0,1));
-					$p->getInventory()->addItem(Item::get(308,0,1));
-					$p->getInventory()->addItem(Item::get(309,0,1));
-					
-					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Samurai".C::DARK_AQUA." Kit!");
-				break;
-			}
-		}
-	}
-	
 	public function removeEffects(PlayerQuitEvent $event){
 	  	$ef = Effect::getEffect(1);
           	$ef1 = Effect::getEffect(8);
@@ -595,6 +482,142 @@ class GameSender extends PluginTask {
 		parent::__construct($plugin);
 	}
   
+  	public function giveRandomKit(PlayerJoinEvent $e){
+		$config = new Config($this->plugin->getDataFolder() . "/config.yml", Config::YAML);
+		if($config->get("RandomKit") === true){	
+			$p = $e->getPlayer();
+			$kit = rand(1,10);
+			switch($kit){
+				case 1:
+					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."VIP+".C::DARK_AQUA." Kit!");
+					$this->p1 = $p;
+				break;
+			
+				case 2:
+					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Beginnerz".C::DARK_AQUA." Kit!");
+					$this->p2 = $p;
+				break;
+			
+				case 3:
+					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Athlete".C::DARK_AQUA." Kit!");
+					$this->p3 = $p;
+				break;
+			
+				case 4:
+					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Rabbit".C::DARK_AQUA." Kit!");
+					$this->p4 = $p;
+				break;
+				
+				case 5:
+					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Midas".C::DARK_AQUA." Kit!");
+					$this->p5 = $p;
+				break;
+			
+				case 6:
+					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Hyper Mongols".C::DARK_AQUA." Kit!");
+					$this->p6 = $p;
+				break;
+				
+				case 7:
+					$p->getInventory()->addItem(Item::get(311,0,1));
+					
+					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Tank".C::DARK_AQUA." Kit!");
+					$this->p7 = $p;
+				break;
+				
+				case 8:
+					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Ninja".C::DARK_AQUA." Kit!");
+					$this->p8 = $p;
+				break;
+				
+				case 9:
+					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Ninja".C::DARK_AQUA." Kit!");
+					$this->p9 = $p;
+				break;
+				
+				case 10:
+					$p->sendMessage(C::DARK_AQUA."You Randomly Got The ".C::YELLOW."Samurai".C::DARK_AQUA." Kit!");
+					$this->p10 = $p;
+				break;
+			}
+		}
+	}
+	
+	public function giveKits(){
+		$p1 = $this->p1;
+		$p2 = $this->p2;
+		$p3 = $this->p3;
+		$p4 = $this->p4;
+		$p5 = $this->p5;
+		$p6 = $this->p6;
+		$p7 = $this->p7;
+		$p8 = $this->p8;
+		$p9 = $this->p9;
+		$p10 = $this->p10;
+		
+		$p1->getInventory()->addItem(Item::get(302,0,1));
+		$p1->getInventory()->addItem(Item::get(303,0,1));
+		$p1->getInventory()->addItem(Item::get(304,0,1));
+		$p1->getInventory()->addItem(Item::get(305,0,1));
+		$p1->getInventory()->addItem(Item::get(279,0,1));
+		
+		$p2->getInventory()->addItem(Item::get(298,0,1));
+		$p2->getInventory()->addItem(Item::get(299,0,1));
+		$p2->getInventory()->addItem(Item::get(300,0,1));
+		$p2->getInventory()->addItem(Item::get(301,0,1));
+		$p2->getInventory()->addItem(Item::get(268,0,1));
+		
+		$effect = Effect::getEffect(1);
+		$effect->setDuration(2184728365782365723642365723652); 
+		$effect->setVisible(true);
+		$effect->setAmplifier(2);
+		$p3->addEffect($effect);
+		$effect2 = Effect::getEffect(8);
+		$effect2->setDuration(2184728365782365723642365723652); 
+		$effect2->setVisible(true);
+		$effect2->setAmplifier(3);
+		$p3->addEffect($effect2);
+		$p3->getInventory()->addItem(Item::get(311,0,1));
+		
+		$ef = Effect::getEffect(8);
+		$ef->setDuration(2184728365782365723642365723652); 
+		$ef->setVisible(true);
+		$ef->setAmplifier(4);
+		$p4->addEffect($ef);
+		$p4->getInventory()->addItem(Item::get(293,0,1));
+		
+		$p5->getInventory()->addItem(Item::get(314,0,1));
+		$p5->getInventory()->addItem(Item::get(315,0,1));
+		$p5->getInventory()->addItem(Item::get(316,0,1));
+		$p5->getInventory()->addItem(Item::get(317,0,1));
+		$p5->getInventory()->addItem(Item::get(283,0,2));
+		$p5->getInventory()->addItem(Item::get(322,0,5));
+		
+		$p6->getInventory()->addItem(Item::get(261,0,1));
+		$p6->getInventory()->addItem(Item::get(262,0,64));
+		$p6->getInventory()->addItem(Item::get(354,0,5));
+		
+		$p7->getInventory()->addItem(Item::get(311,0,1));
+		
+		$p8->getInventory()->addItem(Item::get(293,0,1));
+		$p8->getInventory()->addItem(Item::get(298,0,1));
+		$p8->getInventory()->addItem(Item::get(299,0,1));
+		$p8->getInventory()->addItem(Item::get(300,0,1));
+		$p8->getInventory()->addItem(Item::get(301,0,1));
+		
+		$p9->getInventory()->addItem(Item::get(293,0,1));
+		$p9->getInventory()->addItem(Item::get(298,0,1));
+		$p9->getInventory()->addItem(Item::get(299,0,1));
+		$p9->getInventory()->addItem(Item::get(300,0,1));
+		$p9->getInventory()->addItem(Item::get(301,0,1));
+		
+		
+		$p10->getInventory()->addItem(Item::get(306,0,1));
+		$p10->getInventory()->addItem(Item::get(307,0,1));
+		$p10->getInventory()->addItem(Item::get(308,0,1));
+		$p10->getInventory()->addItem(Item::get(309,0,1));
+	}
+  
 	public function onRun($tick)
 	{
 		$config = new Config($this->plugin->getDataFolder() . "/config.yml", Config::YAML);
@@ -737,6 +760,8 @@ class GameSender extends PluginTask {
                                                                         $pl->sendMessage($this->prefix . C::GREEN . "You have 30 seconds of §bGrace");
                                                                         $pl->sendMessage("§b===============================§r");}
                                                                         $this->refillChests($levelArena);
+                                                                        
+                                                                        $this->giveKits();
 								}
 								$config->set($arena . "StartTime", $timeToStart);
 							}
